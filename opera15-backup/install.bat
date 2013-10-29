1c99b5aa8f215492996b76870c1b75aac83f6529
Robert Gresdal <robertg@opera.com>
2013-10-29 16:19:19 +0100

    Cleaned up some code, added more installation sources.

--- opera15-backup/install.bat
+++ opera15-backup/install.bat
@@ -1,19 +1,21 @@
 rem @echo off
 
+rem init variables
 SET OPERA_NEXT=
 SET OPERA_DEVELOPMENT=
 SEt OPERA_DAILY=
 SET INSTALL_PATH="C:\temp\Opera Development"
 
+rem Load download urls
 (
 	SET /p OPERA_STABLE=
 	SET /p OPERA_NEXT=
+	SET /p OPERA_DEVELOPMENT=
+	SET /p TEST=
 )<download.txt
-@echo on
-if not exist downloads mkdir downloads
-powershell.exe -Command (new-object System.Net.WebClient).DownloadFile("%OPERA_NEXT%", %~dp0./downloads )
-@echo off
-rem wget %OPERA_STABLE%
-echo "done"
+
+IF NOT EXIST downloads MKDIR downloads
+
+powershell.exe -Command ((new-object System.Net.WebClient).DownloadFile("%TEST%", "%~dp0\downloads\foo.bin"))
 
 rem %OPERA% /install /runimmediately /launchopera=0 /installfolder=%INSTALL_PATH% /singleprofile=1 /copyonly=1
