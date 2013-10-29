4dfd047d25b28a6a0805565c9cebaeac87e0cbdd
Robert Gresdal <robert@gresdal.no>
2013-10-29 17:26:05 +0100

    Successfully downloads a file using Windows Powershell.

--- opera15-backup/install.bat
+++ opera15-backup/install.bat
@@ -16,6 +16,12 @@ rem Load download urls
 
 IF NOT EXIST downloads MKDIR downloads
 
-powershell.exe -Command ((new-object System.Net.WebClient).DownloadFile("%TEST%", "%~dp0\downloads\foo.bin"))
+powershell.exe -Command ((new-object System.Net.WebClient).DownloadFile("'%OPERA_DEVELOPMENT%'", "'%~dp0\downloads\install_development.exe'"))
+
+rem powershell -Command (Invoke-WebRequest -uri "'%OPERA_STABLE%'" -OutFile "'%~dp0\downloads\install_stable.exe'")
+
+rem pushd wget
+rem wget -nc "%OPERA_STABLE%" --output-document=../downloads/install_stable.exe
+rem popd
 
 rem %OPERA% /install /runimmediately /launchopera=0 /installfolder=%INSTALL_PATH% /singleprofile=1 /copyonly=1
